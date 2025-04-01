579fddf53c582e5ca646487d44d45908f50a158c
DANDEV2433 <alexdani3344@gmail.com>
2025-04-01 10:26:50 +0200

    task 0

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    {id: 1, firstname: 'John', location: 'Paris'},
+    {id: 2, firstname: 'Alice', location: 'London'},
+    {id: 3, firstname: 'Bob', location: 'New York'}
+  ];
+}
