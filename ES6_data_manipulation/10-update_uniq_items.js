9aabd4cc8be77bf82e9912ebd9b86d92a39a91bd
DANDEV2433 <alexdani3344@gmail.com>
2025-04-02 17:07:48 +0200

    task 10

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,16 @@
+export default function updateUniqueItems(Map, value) {
+  if (!Map) {
+    throw new Error('Cannot process');
+  }
+  const List = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  if (Map.value === 1) {
+    Set.Map(value === 100);
+  }
+  return List;
+}
