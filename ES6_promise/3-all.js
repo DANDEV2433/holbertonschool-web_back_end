4d95b1f5dd93a591f61b6f6fe911dbc290b51a2a
DANDEV2433 <alexdani3344@gmail.com>
2025-04-03 10:26:17 +0200

    task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,9 +1,9 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default async function handleProfileSignup() {
+export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
-    .then(([photo, User]) => {
-      console.log(photo.body, User.firstName, User.lastName);
+    .then(([photo, user]) => {
+      console.log(photo.body, user.firstName, user.lastName);
     })
     .catch(() => {
       console.log('Signup system offline');
