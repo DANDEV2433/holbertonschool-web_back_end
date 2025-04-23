5f42510d302c45daff3184769d6278b2bda17584
DANDEV2433 <alexdani3344@gmail.com>
2025-04-23 14:46:58 +0200

    task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""import using for the floor of the float for rounding down float values"""
+
+import math
+
+""""""
+"""
+function witch returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """take a float n as argument and returns the floor of the float."""
+    return math.floor(n)
