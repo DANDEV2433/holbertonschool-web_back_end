e1c0d426f00da6c127aece303138170238f4eb88
DANDEV2433 <alexdani3344@gmail.com>
2025-04-23 15:45:07 +0200

    task 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-
+"""following variables with the specified values"""
+"""variable a is an integer with the value 1"""
 a: int = 1
+"""variable pi is a float with the value 3.14"""
 pi: float = 3.14
+"""variable i_understand_annotations is a boolean with the value True"""
 i_understand_annotations: bool = True
+"""variable school is a string with the value 'Holberton'"""
 school: str = "Holberton"
