6607c60a0fff12eec5d566240a68ca2afde208b4
DANDEV2433 <alexdani3344@gmail.com>
2025-04-23 19:14:50 +0200

    task 7 not finish

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Import List for type hinting a list of floats (List[float])
+"""
+from typing import Union
+
+
+"""
+function to_kv that takes a string k and an int OR float v
+as arguments and returns a tuple. The first element of the tuple is the string k.
+The second element is the square of the int/float v and should be annotated as a float.
+"""
+
+
+def to_kv(K: str, v: Union[int, float]) -> Union[str, float]:
+    """
+    a list mxd_lst of integers and floats
+    and returns their sum as a float.
+    """
+    return sum(mxd_lst)
