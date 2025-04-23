2a1206add935006095f17b14abdcdf97de5647f5
DANDEV2433 <alexdani3344@gmail.com>
2025-04-23 14:19:58 +0200

    task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+function to add two string and return a concatenated string.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Add two string and return the concatenated result of them."""
+    return str1 + str2
