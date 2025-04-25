b27166b3ecec5c2c9db8f6321cbd715ee106c77f
DANDEV2433 <alexdani3344@gmail.com>
2025-04-25 09:41:36 +0200

    task 1

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 3
+"""
+ascyncio can be used to write asynchronous code that can wait
+without blocking and run several tasks at the same time.
+random can be used to generate random values.
+"""
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    function wait_n which takes in 2 arguments:
+    n: number of times to call wait_random
+    max_delay: maximum delay to wait for
+    Returns a list of all the delays (float values)
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = []
+    for completed_tasks in asyncio.as_completed(tasks):
+        delay = await completed_tasks
+        delays.append(delay)
+    return delays
