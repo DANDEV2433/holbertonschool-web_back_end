00a08cc7d7ff6bace441c3ea1bdfacd32ffb2875
DANDEV2433 <alexdani3344@gmail.com>
2025-04-28 23:18:04 +0200

    task 2

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Importing the async_comprehension function from the 1-async_comprehension
+ascyncio can be used to write asynchronous code that can wait
+without blocking and run several tasks at the same time.
+time can be used to measure an approximate elapsed time
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """
+    Measure the total runtime of 4 parallel comprehensions
+    and return the total runtime.
+    Since async_generator generates 10 iterations with a 1 second
+    wait each time, it takes a minimum of 10 seconds for each complete call,
+    even with 4 calls because they are made in parallel with asyncio.gather.
+    """
+    start = time.perf_counter()
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+    end = time.perf_counter()
+    return end - start
