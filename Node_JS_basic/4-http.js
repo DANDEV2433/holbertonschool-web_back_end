0afb44e17bfb1e889cfcc29f8e6d32034217a5fc
DANDEV2433 <alexdani3344@gmail.com>
2025-05-21 11:09:52 +0200

    task 4

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = http.createServer((req, res) => {
+    res.setHeader('content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+module.exports = app;
